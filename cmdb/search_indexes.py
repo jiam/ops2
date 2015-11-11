2fbd33000d5a6a16275d0acc9f853a0d4e0e888c
root <root@cmdb.ops.creditease.corp>
2015-11-11 12:00:34 +0800

    修改高级查询 search_index

--- cmdb/search_indexes.py
+++ cmdb/search_indexes.py
@@ -10,16 +10,22 @@ class hostvirtualIndex(indexes.SearchIndex,indexes.Indexable):
     physical_host_ip = indexes.CharField(model_attr='Physical_Host_IP',null=True)
     user = indexes.CharField(model_attr='User',null=True)
     useinfo = indexes.CharField(model_attr='Use_Info',null=True)
-    service_name = indexes.CharField()
-    department_name = indexes.CharField()
+    service_name = indexes.CharField(null=True)
+    department_name = indexes.CharField(null=True)
     def get_model(self):
         return HostVirtual
     def index_queryset(self,using=None):
         return self.get_model().objects.all()
     def prepare_department_name(self, obj):
-        return obj.department.Department_Name
+        if obj.department == None:
+            pass
+        else:
+            return obj.department.Department_Name
     def prepare_service_name(self, obj):
-        return obj.service.Service_Name
+        if obj.service == None:
+            pass
+        else:
+            return obj.service.Service_Name
 
 class hostphysicalIndex(indexes.SearchIndex,indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -27,15 +33,15 @@ class hostphysicalIndex(indexes.SearchIndex,indexes.Indexable):
     sn = indexes.CharField(model_attr='SN')
     assert_sn = indexes.CharField(model_attr='Asset_SN')
     manage_ip = indexes.CharField(model_attr='Manage_IP')
-    rac_ip = indexes.CharField(model_attr='RAC_IP')
+    rac_ip = indexes.CharField(model_attr='RAC_IP',null=True)
     vip = indexes.CharField(model_attr='VIP',null=True)
     nas_ip = indexes.CharField(model_attr='NAS_IP',null=True)
     user = indexes.CharField(model_attr='User',null=True)
-    idc_name = indexes.CharField()
-    vendor_name = indexes.CharField()
-    model_name = indexes.CharField()
-    service_name = indexes.CharField()
-    department_name = indexes.CharField()
+    idc_name = indexes.CharField(null=True)
+    vendor_name = indexes.CharField(null=True)
+    model_name = indexes.CharField(null=True)
+    service_name = indexes.CharField(null=True)
+    department_name = indexes.CharField(null=True)
     useinfo = indexes.CharField(model_attr='UseInfo',null=True)
     
     def get_model(self):
@@ -43,15 +49,30 @@ class hostphysicalIndex(indexes.SearchIndex,indexes.Indexable):
     def index_queryset(self,using=None):
         return self.get_model().objects.select_related('Manag_IP').all()
     def prepare_idc_name(self, obj):
-        return obj.idc.IDC_Name
+        if  obj.idc == None:
+            pass
+        else:
+            return obj.idc.IDC_Name
     def prepare_vendor_name(self, obj):
-        return obj.vendor.Vendor_Name
+        if obj.vendor == None:
+            pass
+        else:
+            return obj.vendor.Vendor_Name
     def prepare_model_name(self, obj):
-        return obj.model.Model_Name
+        if obj.model == None:
+            pass
+        else:
+            return obj.model.Model_Name
     def prepare_department_name(self, obj):
-        return obj.department.Department_Name
+        if obj.department == None:
+            pass
+        else:
+            return obj.department.Department_Name
     def prepare_service_name(self, obj):
-        return obj.service.Service_Name
+        if obj.service == None:
+            pass
+        else:
+            return obj.service.Service_Name
 class application(indexes.SearchIndex,indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     service = indexes.CharField(model_attr='service')
